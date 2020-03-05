1de4fd50ef75cd5916cb7a6455d1edb3caae194c
Gabriel Oliveira <gabriel.oliveira@engdb.com.br>
2020-03-05 20:20:39 -0300

    Setup inicial

---
+++ src/main/java/br/com/brazilcode/cb/purchase/service/CbPurchaseServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.brazilcode.cb.purchase.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbPurchaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbPurchaseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brazilcode/cb/purchase/service/CbPurchaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brazilcode.cb.purchase.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbPurchaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
