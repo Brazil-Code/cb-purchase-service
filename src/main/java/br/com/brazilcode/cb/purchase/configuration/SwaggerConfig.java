47dff19fb7c0dd07d7451b7e393aab6385b393ce
Gabriel Oliveira <gabriel.oliveira@engdb.com.br>
2020-03-19 22:12:34 -0300

    Aplicando Swagger na API

--- src/main/java/br/com/brazilcode/cb/purchase/configuration/SwaggerConfig.java
+++ src/main/java/br/com/brazilcode/cb/purchase/configuration/SwaggerConfig.java
@@ -1,10 +1,15 @@
-package br.com.brazilcode.cb.purchase;
+package br.com.brazilcode.cb.purchase.configuration;
+
+import java.util.ArrayList;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -26,7 +31,22 @@ public class SwaggerConfig {
           .select()
           .apis(RequestHandlerSelectors.basePackage("br.com.brazilcode.cb.purchase.controller"))
           .paths(PathSelectors.any())
-          .build();
+          .build()
+          .apiInfo(metaInfo());
     }
+
+	@SuppressWarnings("rawtypes")
+	private ApiInfo metaInfo() {
+		return new ApiInfo(
+				"Purchase Request REST API", 
+				"REST API for the purchase module of the Clean Budget project", 
+				"1.0", 
+				"Terms of Service",
+				new Contact("Gabriel Guarido", "https://www.linkedin.com/in/gabriel-guarido-oliveira/", 
+						"gabrielguarido.oliveira@gmail.com"),
+				"MIT License",
+				"https://opensource.org/licenses/MIT", new ArrayList<VendorExtension>()
+		);
+	}
 
 }
