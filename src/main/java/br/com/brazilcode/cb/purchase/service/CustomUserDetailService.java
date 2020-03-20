954bbbeea4b6b13edfa7c855053a2a5262134df2
Gabriel Oliveira <gabriel.oliveira@engdb.com.br>
2020-03-20 20:44:09 -0300

    Implementando Lambda

--- src/main/java/br/com/brazilcode/cb/purchase/service/CustomUserDetailService.java
+++ src/main/java/br/com/brazilcode/cb/purchase/service/CustomUserDetailService.java
@@ -1,7 +1,6 @@
 package br.com.brazilcode.cb.purchase.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -33,8 +32,7 @@ public class CustomUserDetailService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = Optional.ofNullable(userRepository.findByUsername(username))
-				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+		User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
 		List<GrantedAuthority> authorityListUser = AuthorityUtils.createAuthorityList("ROLE_USER");
 		return new org.springframework.security.core.userdetails.User(user.getUsername(), "000", authorityListUser);
 	}
